fb778a3845e95144af46eca2bab2337e50d898eb
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-06-11 09:19:02 +0530

    initial commit

---
+++ src/test/java/com/basawaraj/dsa/DsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basawaraj.dsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
