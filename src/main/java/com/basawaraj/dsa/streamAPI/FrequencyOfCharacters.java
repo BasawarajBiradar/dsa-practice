8bc9266ee26fd1c837ad7326da1da7a9e9327997
BasawarajBiradar <basawarajbiradar007@gmail.com>
2026-06-14 13:06:21 +0530

    stream api : frequency of the elements

---
+++ src/main/java/com/basawaraj/dsa/streamAPI/FrequencyOfCharacters.java
@@ -0,0 +1,17 @@
+package com.basawaraj.dsa.streamAPI;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyOfCharacters {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        Map<Character, Long> frequency  =
+                str.chars().mapToObj(s -> (char) s)
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(frequency);
+    }
+
+}
